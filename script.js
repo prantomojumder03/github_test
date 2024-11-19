ca0ee52015865c1b994683849b4ee567e2965f7c
prantomojumder03 <iplpranto2@gmail.com>
2024-11-20 00:44:50 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// "use strict";
+
+// Binary operator
+// console.log(2+5); // (2 + 5) ---> Binary operand means two operand
+
+// Unary operator
+// let firstNumber = 5;
+
+// console.log(firstNumber);
+
+// firstNumber = -firstNumber;
+
+// console.log(firstNumber);
+
+// let secondNumber = -5;
+
+// console.log(secondNumber);
+
+// Binary operator
+let firstNumber = 5;
+
+let secondNumber = 2;
+
+let substraction = firstNumber - secondNumber;
+let addition = firstNumber + secondNumber;
+let multiplication = firstNumber * secondNumber;
+let divider = firstNumber / secondNumber;
+let reminder_or_modulus = firstNumber % secondNumber;
+let exponentiation = firstNumber ** secondNumber;
+
+console.log(substraction);
+console.log(addition);
+console.log(multiplication);
+console.log(divider);
+console.log(reminder_or_modulus);
+console.log(exponentiation);
